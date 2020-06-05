db00c5761919de6ffcdf3c759100bfcc64372791
jiajinbu <jiajinbu@users.noreply.github.com>
2020-06-05 10:33:05 +0800

    Update fast5readwrapper.py

--- polyacaller/fast5readwrapper.py
+++ polyacaller/fast5readwrapper.py
@@ -144,6 +144,22 @@ class Fast5ReadWrapper:
         |766  |     1    |  G | 12         |
         |778  |     2    |  C | 2          |
         ------------------------------------
+        
+        Note:
+        pA_val = scale * (raw + offset)
+        
+        raw (event) ---------------------------------------------------------------------------
+        
+        frist_template_start    |
+        
+        move (stride = 4) index 0    1    2    3    4 ....
+                                |    |    |    |    | ....
+                          value 1    0    0    0    1 ....
+        base              index 0                   1 ...
+                          value C                   T ..
+        
+        
+       
         """
         try:
             read = self.get_read(self.read_id)
